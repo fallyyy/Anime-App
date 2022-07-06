b53725228389cf3c02583ece4a56d01efcc13d80
fallyyy <dollylally@mail.ru>
2022-07-06 20:01:45 +0400

    Фикс.

--- Pods/Target Support Files/Pods-AnimeApp/Pods-AnimeApp-umbrella.h
+++ Pods/Target Support Files/Pods-AnimeApp/Pods-AnimeApp-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_CharactersListVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_CharactersListVersionString[];
+FOUNDATION_EXPORT double Pods_AnimeAppVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_AnimeAppVersionString[];
 
--- Pods/Target Support Files/Pods-CharactersList/Pods-CharactersList-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CharactersList : NSObject
-@end
-@implementation PodsDummy_Pods_CharactersList
-@end
